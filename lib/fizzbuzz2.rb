4e0b0e4cfed370f3b3dede8cf095f514193b852b
RSijelmass <R.Sijelmass@gmail.com>
2017-05-03 17:03:25 +0100

    Prints fizzbuzz for multiples of both 3 and 5, refactors code

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -1,8 +1,6 @@
 def fizzbuzz(num)
   str = ''
-  if num % 3 == 0
-    str << 'fizz'
-  elsif num % 5 == 0
-    str << 'buzz'
-  end
+  str << 'fizz' if num % 3 == 0
+  str << 'buzz' if num % 5 == 0
+  return str
 end
--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz2' do
   it 'returns "buzz" if a multiple of 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" if a multiple of 3 & 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
