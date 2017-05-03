3fc2547a3898856cfe65a8efd324d674fe3655da
RSijelmass <R.Sijelmass@gmail.com>
2017-05-03 16:56:13 +0100

    Only prints buzz for multiples of 5

--- lib/fizzbuzz2.rb
+++ lib/fizzbuzz2.rb
@@ -2,5 +2,7 @@ def fizzbuzz(num)
   str = ''
   if num % 3 == 0
     str << 'fizz'
+  elsif num % 5 == 0
+    str << 'buzz'
   end
 end
--- spec/fizzbuzz2_spec.rb
+++ spec/fizzbuzz2_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz2' do
   it 'returns "fizz" if a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" if a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
